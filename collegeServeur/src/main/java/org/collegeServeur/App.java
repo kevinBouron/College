e14805c6b70b77b3a9689e15120cc9df33241763
kevinBouron <kb.bouron@gmail.com>
2019-04-16 16:53:17 +0200

    first commit

---
+++ collegeServeur/src/main/java/org/collegeServeur/App.java
@@ -0,0 +1,13 @@
+package org.collegeServeur;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
