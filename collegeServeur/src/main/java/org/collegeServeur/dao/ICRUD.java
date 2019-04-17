8045bed9780665672406031f1d0e0609e7053e9a
IN-NN-005 <IN-NN-005@192.168.1.164>
2019-04-17 11:55:08 +0200

    icrud

--- collegeServeur/src/main/java/org/collegeServeur/dao/ICRUD.java
+++ collegeServeur/src/main/java/org/collegeServeur/dao/ICRUD.java
@@ -1,5 +1,11 @@
 package org.collegeServeur.dao;
 
+import java.util.List;
+
 public interface ICRUD <T> {
 
+	public void create(T t);
+	public void update(T t);
+	public void delete(T t);
+	public List<T> display();
 }
