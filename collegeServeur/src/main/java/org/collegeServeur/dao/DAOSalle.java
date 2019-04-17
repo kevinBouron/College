7bb355da33fa73f547b0eae49d1e15287cd79e81
MarineLC <49683302+MarineLC@users.noreply.github.com>
2019-04-17 12:15:44 +0200

    commit personne+salle

--- collegeServeur/src/main/java/org/collegeServeur/dao/DAOSalle.java
+++ collegeServeur/src/main/java/org/collegeServeur/dao/DAOSalle.java
@@ -1,5 +1,7 @@
 package org.collegeServeur.dao;
 
+import java.util.List;
+
 import org.collegeServeur.entities.Salle;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,29 @@ public class DAOSalle implements IDAOSalle{
 	
 	@Autowired
 	private SessionFactory sessionFactory;
+
+	public void create(Salle t) {
+
+		sessionFactory.getCurrentSession().merge(t);
+		
+	}
+
+	public void update(Salle t) {
+
+		sessionFactory.getCurrentSession().update(t);
+		
+	}
+
+	public void delete(Salle t) {
+
+		sessionFactory.getCurrentSession().delete(t);
+		
+	}
+
+	public List<Salle> display() {
+
+		return sessionFactory.getCurrentSession().createQuery("from Salle s").list();
+	}
 
 
 }
